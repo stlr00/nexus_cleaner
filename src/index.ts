dbb4585c05213eff8cfe0b29716bae16503f004d
Alexander Vesyolkin <sashaasdzxc@gmail.com>
2022-07-19 20:38:55 +0300

    fix index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
-// const getComponentList = require("./requests/getList");
-// const sortContainers = require('./sort/sort')
-// const deleteList = require('./sort/listForDelete')
-// const deleteContainers = require("./requests/deleteRequest");
+import {getComponentList} from "./requests/getList";
+import {sortContainers} from "./sort/sort";
+import {deleteList} from "./sort/listForDelete";
+import {deleteContainers} from "./requests/deleteRequest";
 
-// async function run() {
-//     const listOfContainers = await getComponentList()
-//     const sorted = sortContainers(listOfContainers)
-//     const idsForDelete = deleteList(sorted)
-//     await deleteContainers(idsForDelete)
-// }
+async function run() {
+    const listOfContainers = await getComponentList()
+    const sorted = sortContainers(listOfContainers)
+    const idsForDelete = deleteList(sorted)
+    await deleteContainers(idsForDelete)
+}
 
-// run()
+run()
